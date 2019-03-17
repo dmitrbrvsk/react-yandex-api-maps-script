004f83410a3999fe00ddce889a6a06053eff7973
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2019-03-17 16:40:08 +0300

    fix pass apikey to script

--- src/YandexApiMapsScript.js
+++ src/YandexApiMapsScript.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { string } from 'prop-types'
 
 const YandexApiMapsScript = apikey => {
-  return <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=${apikey}" type="text/javascript"/>
+  return `<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=${apikey}" type="text/javascript" />`
 }
 
 YandexApiMapsScript.propTypes = {
