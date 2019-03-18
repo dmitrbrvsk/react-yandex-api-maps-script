ad4955f528b74dd5a784e24a1745892d6157f724
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2019-03-18 14:53:15 +0300

    fix inject inline script

--- src/YandexApiMapsScript.js
+++ src/YandexApiMapsScript.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ScriptTag from 'react-script-tag'
 import { string } from 'prop-types'
 
 const YandexApiMapsScript = apikey => {
-  return `<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=${apikey}" type="text/javascript" />`
+  const src = `https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=${apikey}`
+  return  (
+    <ScriptTag src={src} type="text/javascript" />
+  )
 }
 
 YandexApiMapsScript.propTypes = {
