035add7d46f0a8554f90d8795238b6880af686d3
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2019-03-20 02:23:14 +0300

    add opportunity set type (free/paid) API

--- src/YandexApiMapsScript.js
+++ src/YandexApiMapsScript.js
@@ -2,15 +2,26 @@ import React from 'react'
 import ScriptTag from 'react-script-tag'
 import { string } from 'prop-types'
 
-const YandexApiMapsScript = ({apikey}) => {
-  const src = `https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=${apikey}`
-  return  (
+const srcApi = {
+  freeVersion: 'https://api-maps.yandex.ru',
+  paidVersion: 'https://enterprise.api-maps.yandex.ru',
+}
+
+const YandexApiMapsScript = ({apikey, srcApi}) => {
+  const src = `${srcApi}/2.1/?lang=ru_RU&amp;apikey=${apikey}`
+
+  return (
     <ScriptTag src={src} type="text/javascript" />
   )
 }
 
 YandexApiMapsScript.propTypes = {
+  typeSrc: string,
   apikey: string.isRequired,
+}
+
+YandexApiMapsScript.defaultProps = {
+  typeSrc: srcApi.freeVersion,
 }
 
 export default YandexApiMapsScript
