99720eafc5ea23454c8fc0cfadc7adb44a3dea82
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2019-03-20 02:01:06 +0300

    fix pass params

--- src/YandexApiMapsScript.js
+++ src/YandexApiMapsScript.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ScriptTag from 'react-script-tag'
 import { string } from 'prop-types'
 
-const YandexApiMapsScript = apikey => {
+const YandexApiMapsScript = ({apikey}) => {
   const src = `https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=${apikey}`
   return  (
     <ScriptTag src={src} type="text/javascript" />
